136346881473c40cfce3410638324cf2852f1d48
gandradep <eventsonido@gmail.com>
2023-02-20 21:23:50 -0600

    Creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'categories#index'
+  root 'home#index'
   resources :categories, only: %i[index show new create destroy] do
     resources :transactions, only: %i[ new create destroy ]
   end
